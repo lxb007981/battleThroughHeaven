0d0aca23ebb0bb96d6c5a7c12e9e7f44e7bb1743
lxb007981 <lxb007981@hotmail.com>
2020-08-08 15:31:31 +0800

    init

---
+++ battleThroughHeaven.py
@@ -0,0 +1,55 @@
+from os import system
+system('!nvidia-smi')
+
+
+from transformers import AlbertTokenizer
+tokenizer = AlbertTokenizer('doupoModel.model')
+
+from transformers import GPT2Config
+from transformers import GPT2LMHeadModel
+config = GPT2Config(vocab_size=52000)
+device = "cuda"
+model = GPT2LMHeadModel(config=config)
+model.to(device)
+model.train()
+print(model.num_parameters())
+
+
+from transformers import LineByLineTextDataset
+dataset = LineByLineTextDataset(
+    tokenizer=tokenizer,
+    file_path="./doupo.txt",
+    block_size=128,
+)
+
+from transformers import DataCollatorForLanguageModeling
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+)
+
+from transformers import Trainer, TrainingArguments
+
+training_args = TrainingArguments(
+    output_dir="./doupoGPT2",
+    overwrite_output_dir=True,
+    logging_steps=50,
+    num_train_epochs=10,
+    per_device_train_batch_size=64,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=dataset,
+    prediction_loss_only=True,
+)
+
+trainer.train()
+
+trainer.save_model("./doupoGPT2")
+
+system('zip -q -r doupoGPT2.zip doupoGPT2/')
